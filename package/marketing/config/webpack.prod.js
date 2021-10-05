3cbf450a0b302d43eb8843d27c5a895036276198
jaisinghrathore <jairqthore@gmail.com>
2021-10-05 12:04:54 +0530

    Created containerr dep

---
+++ package/marketing/config/webpack.prod.js
@@ -0,0 +1,24 @@
+const {merge} = require('webpack-merge');
+const ModuleFedrationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
+
+
+const prodConfig = {
+    mode : 'production',
+    output:{
+        filename: '[name].[contenthash].js'
+    },
+    plugins : [
+        new ModuleFedrationPlugin({
+            name:"marketing",
+            filename: 'remoteEntry.js',
+            exposes: {
+                './marketingIndex' : './src/bootstrap'
+            },
+            shared : packageJson.dependencies
+        }),
+    ]
+}
+
+module.exports = merge(commonConfig,prodConfig);
